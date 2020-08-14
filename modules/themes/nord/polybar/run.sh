4581f810dad9b11cc04c300387492f075bd69f6c
Justus K <justus.k@protonmail.com>
2020-08-14 19:03:49 +0200

    Add polybar config

--- modules/themes/nord/polybar/run.sh
+++ modules/themes/nord/polybar/run.sh
@@ -3,4 +3,6 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar bar >$XDG_DATA_HOME/polybar.log 2>&1 &
+polybar -rq music &
+polybar -rq tray &
+polybar -rq ws &

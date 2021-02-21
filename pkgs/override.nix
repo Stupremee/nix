246ad9a9d4781a2aab33793954de8aa24528c344
Justus K <justus.k@protonmail.com>
2021-02-21 13:03:29 +0100

    chore: update flake inputs

--- pkgs/override.nix
+++ pkgs/override.nix
@@ -1,5 +1,5 @@
 # Packages that are imported inside this file,
 # will be pulled from the `unstable` nixpkgs.
 pkgs: final: prev: {
-  inherit (pkgs) manix rust-analyzer firefox-wayland;
+  inherit (pkgs) vimPlugins manix rust-analyzer firefox-wayland;
 }

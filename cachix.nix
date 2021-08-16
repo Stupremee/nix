e16baec2830d5f18ed3b3d9ffe5c58c8bedcbcde
Justus K <justus.k@protonmail.com>
2021-08-16 10:12:48 +0200

    feat: add veloren cachix cache

--- cachix.nix
+++ cachix.nix
@@ -1,10 +1,15 @@
 { ... }: {
   nix = {
     binaryCaches =
-      [ "https://cache.nixos.org/" "https://nixpkgs-wayland.cachix.org" ];
+      [
+        "https://cache.nixos.org/"
+        "https://nixpkgs-wayland.cachix.org"
+        "https://veloren-nix.cachix.org"
+      ];
 
     binaryCachePublicKeys = [
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      "veloren-nix.cachix.org-1:zokfKJqVsNV6kI/oJdLF6TYBdNPYGSb+diMVQPn/5Rc="
     ];
   };
 }

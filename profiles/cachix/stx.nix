77ab25aaa15e921e2b46bb645b2d92e65f0b47fe
Justus K <justus.k@protonmail.com>
2021-12-03 23:17:23 +0100

    fix(ci): use own cachix and run on new branch

---
+++ profiles/cachix/stx.nix
@@ -0,0 +1,10 @@
+{
+  nix = {
+    binaryCaches = [
+      "https://stx.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "stx.cachix.org-1:mCLmdjTaKn3704lctpvqOgc0uvmu0NnIUP3/d5g2aZ0="
+    ];
+  };
+}

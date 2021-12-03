ee8acc1fd710847d5272042dded37a5c72e0e49a
Justus K <justus.k@protonmail.com>
2021-12-04 00:27:36 +0100

    feat(users): add proper git profile

--- users/profiles/git/default.nix
+++ users/profiles/git/default.nix
@@ -1,45 +1,38 @@
-{
+{ pkgs, ... }: {
   programs.git = {
     enable = true;
+    lfs.enable = true;
 
     extraConfig = {
-      pull.rebase = false;
+      pull.rebase = true;
     };
+
+    userName = "Justus K";
+    userEmail = "justus.k@protonmail.com";
+
+    signing.key = "D54A1CD51376F46C";
+    signing.signByDefault = true;
 
     aliases = {
       a = "add -p";
       co = "checkout";
-      cob = "checkout -b";
-      f = "fetch -p";
       c = "commit";
       p = "push";
-      ba = "branch -a";
-      bd = "branch -d";
-      bD = "branch -D";
-      d = "diff";
-      dc = "diff --cached";
-      ds = "diff --staged";
-      r = "restore";
-      rs = "restore --staged";
-      st = "status -sb";
+      df = "diff";
 
       # reset
       soft = "reset --soft";
       hard = "reset --hard";
-      s1ft = "soft HEAD~1";
-      h1rd = "hard HEAD~1";
 
       # logging
-      lg =
-        "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      plog =
-        "log --graph --pretty='format:%C(red)%d%C(reset) %C(yellow)%h%C(reset) %ar %C(green)%aN%C(reset) %s'";
-      tlog =
-        "log --stat --since='1 Day Ago' --graph --pretty=oneline --abbrev-commit --date=relative";
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      plog = "log --graph --pretty='format:%C(red)%d%C(reset) %C(yellow)%h%C(reset) %ar %C(green)%aN%C(reset) %s'";
+      tlog = "log --stat --since='1 Day Ago' --graph --pretty=oneline --abbrev-commit --date=relative";
       rank = "shortlog -sn --no-merges";
-
-      # delete merged branches
-      bdm = "!git branch --merged | grep -v '*' | xargs -n 1 git branch -d";
     };
+
+    delta.enable = true;
   };
+
+  home.packages = with pkgs; [ gh ];
 }
--- users/root/default.nix
+++ users/root/default.nix
@@ -1,5 +1,7 @@
 { ... }:
-# recommend using `hashedPassword`
 {
-  users.users.root.password = "";
+  users.users.root = {
+    hashedPassword = "$6$kXVCr27G60l.a7z9$4iRje1ZUDkEgrxNXwV6gF5cLoIheXNflPwNwQssLufYESkrH1s2wW0nWtC.y2rVtdmRusCHP2GIgKS/7DQ6cG0";
+    openssh.authorizedKeys.keyFiles = [ ../../secrets/keys/yubikey.pub ];
+  };
 }

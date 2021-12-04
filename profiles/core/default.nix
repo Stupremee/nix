8b452a2d6910da788bc29c7786d4da0a4c6100e1
Justus K <justus.k@protonmail.com>
2021-12-04 12:37:41 +0100

    feat(profiles): rework core profile to use zsh

--- profiles/core/default.nix
+++ profiles/core/default.nix
@@ -17,30 +17,35 @@ in
   ];
 
   environment = {
-
     systemPackages = with pkgs; [
-      binutils
-      coreutils
-      curl
-      direnv
-      dnsutils
-      dosfstools
-      fd
-      git
       bottom
-      gptfdisk
-      iputils
-      jq
-      manix
-      moreutils
       nix-index
-      nmap
-      ripgrep
       skim
       tealdeer
-      usbutils
-      utillinux
-      whois
+      coreutils-full
+      direnv
+      ripgrep
+      ripgrep-all
+      tealdeer
+      fd
+      fzf
+      bat
+      exa
+      file
+      pulsemixer
+      procs
+      tokei
+      jq
+      manix
+      git
+      neovim
+      nixos-option
+      dogdns
+
+      man-db
+      man-pages
+      man-pages-posix
+      stdmanpages
     ];
 
     shellInit = ''
@@ -54,58 +59,30 @@ in
       let ifSudo = lib.mkIf config.security.sudo.enable;
       in
       {
-        # quick cd
-        ".." = "cd ..";
-        "..." = "cd ../..";
-        "...." = "cd ../../..";
-        "....." = "cd ../../../..";
+        # exa
+        ls = "exa --group-directories-first";
+        l = "exa -1";
+        ll = "exa -lg";
+        la = "exa -la";
 
-        # git
         g = "git";
-
-        # grep
-        grep = "rg";
-        gi = "grep -i";
+        ps = "procs";
+        top = "btm";
 
         # internet ip
-        myip = "dig +short myip.opendns.com @208.67.222.222 2>&1";
+        myip = "dog myip.opendns.com @208.67.222.222";
 
         # nix
-        n = "nix";
-        np = "n profile";
-        ni = "np install";
-        nr = "np remove";
-        ns = "n search --no-update-lock-file";
-        nf = "n flake";
-        nepl = "n repl '<nixpkgs>'";
-        srch = "ns nixos";
-        orch = "ns override";
-        nrb = ifSudo "sudo nixos-rebuild";
         mn = ''
-          manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | sk --preview="manix '{}'" | xargs manix
+          manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' |  sk --preview='manix {}' | sed "s/'/\\\\'/g" | xargs manix
         '';
 
         # fix nixos-option
         nixos-option = "nixos-option -I nixpkgs=${self}/lib/compat";
 
-        # sudo
-        s = ifSudo "sudo -E ";
-        si = ifSudo "sudo -i";
-        se = ifSudo "sudoedit";
-
-        # top
-        top = "btm";
-
         # systemd
         ctl = "systemctl";
-        stl = ifSudo "s systemctl";
-        utl = "systemctl --user";
-        ut = "systemctl --user start";
-        un = "systemctl --user stop";
-        up = ifSudo "s systemctl start";
-        dn = ifSudo "s systemctl stop";
-        jtl = "journalctl";
-
+        stl = ifSudo "sudo systemctl";
       };
   };
 
@@ -113,26 +90,23 @@ in
     fonts = with pkgs; [ powerline-fonts dejavu_fonts ];
 
     fontconfig.defaultFonts = {
-
       monospace = [ "DejaVu Sans Mono for Powerline" ];
-
       sansSerif = [ "DejaVu Sans" ];
-
     };
   };
 
+  # Set timezone and locale
+  i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = "Europe/Berlin";
+
   nix = {
-
     autoOptimiseStore = true;
-
     gc.automatic = true;
-
     optimise.automatic = true;
 
     useSandbox = true;
 
     allowedUsers = [ "@wheel" ];
-
     trustedUsers = [ "root" "@wheel" ];
 
     extraOptions = ''
@@ -144,12 +118,24 @@ in
 
   };
 
-  programs.bash = {
-    promptInit = ''
-      eval "$(${pkgs.starship}/bin/starship init bash)"
+  # Configure zsh shell.
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh = {
+    enable = true;
+    enableBashCompletion = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+
+    shellInit = ''
+      if [ "$0" = "zsh-no-vim" ]; then
+        bindkey -e
+      else
+        bindkey -v
+      fi
     '';
-    interactiveShellInit = ''
-      eval "$(${pkgs.direnv}/bin/direnv hook bash)"
+
+    promptInit = ''
+      eval "$(${pkgs.starship}/bin/starship init zsh)"
     '';
   };
 
@@ -160,5 +146,4 @@ in
   };
 
   services.earlyoom.enable = true;
-
 }

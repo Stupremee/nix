abeb19a5f36e0bf4c75946d774f824baaab4a753
Justus K <justus.k@protonmail.com>
2024-10-28 14:43:13 +0100

    fix boot logs bleeding into tuigreet

--- modules/wayland/default.nix
+++ modules/wayland/default.nix
@@ -45,6 +45,19 @@ in {
       };
     };
 
+    # Fix for boot logs bleeding into the tuigreet
+    # https://github.com/apognu/tuigreet/issues/68#issuecomment-2001807691
+    systemd.services.greetd.serviceConfig = {
+      Type = "idle";
+      StandardInput = "tty";
+      StandardOutput = "tty";
+      StandardError = "journal"; # Without this errors will spam on screen
+      # Without these bootlogs will spam on screen
+      TTYReset = true;
+      TTYVHangup = true;
+      TTYVTDisallocate = true;
+    };
+
     security = {
       polkit.enable = true;
       rtkit.enable = true;

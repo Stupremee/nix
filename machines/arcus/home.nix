9276f390b6d850e7ffbde2f4f39830a67dd047f5
Justus K <justus.k@protonmail.com>
2025-01-09 16:01:41 +0100

    arcus: init machine

---
+++ machines/arcus/home.nix
@@ -0,0 +1,30 @@
+{...}: {
+  my = {
+    alacritty.enable = true;
+    zsh.enable = true;
+
+    mako.enable = true;
+
+    hyprland = {
+      enable = true;
+
+      sensitivity = "0.0";
+
+      monitors = {
+        "DP-5" = {
+          position = "0x0";
+          resolution = "3840x2160@60";
+          scale = "1.5";
+        };
+
+        "DP-6" = {
+          position = "2560x0";
+          resolution = "3840x2160@60";
+          scale = "1.5";
+        };
+
+        "Unknown-1".disable = true;
+      };
+    };
+  };
+}

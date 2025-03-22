f759ca0d980fdc0414c4e66821207e8954f07cb1
Justus K <justus.k@protonmail.com>
2025-03-22 17:39:59 +0100

    paperless: allow login via authelia

--- modules/oidc/default.nix
+++ modules/oidc/default.nix
@@ -13,6 +13,40 @@ in
 {
   options.my.oidc = {
     enable = mkEnableOption "Enable authelia OIDC provider and LLDAP";
+
+    clients = mkOption {
+      default = { };
+      type = types.attrsOf (
+        types.submodule (
+          { ... }:
+          {
+            options = {
+              name = mkOption {
+                type = types.str;
+              };
+
+              secret = mkOption {
+                type = types.str;
+              };
+
+              authorization_policy = mkOption {
+                type = types.str;
+                default = "one_factor";
+              };
+
+              scopes = mkOption {
+                type = types.listOf types.str;
+                default = ["openid" "profile"];
+              };
+
+              redirect_uris = mkOption {
+                type = types.listOf types.str;
+              };
+            };
+          }
+        )
+      );
+    };
   };
 
   config = mkIf cfg.enable {
@@ -24,14 +58,15 @@ in
       } // attrs;
     in {
       lldap-env.rekeyFile = ../../secrets/lldap.env.age;
+      authelia-private-key = secret { rekeyFile = ../../secrets/authelia-private-key.age; };
 
       authelia-ldap-user-password = secret {
         rekeyFile = ../../secrets/authelia-ldap-user-password.age;
       };
-
       authelia-storage-encryption-key = secret { generator.script = "alnum"; };
       authelia-jwt-secret = secret { generator.script = "alnum"; };
       authelia-session-secret = secret { generator.script = "alnum"; };
+      authelia-hmac-secret = secret { generator.script = "alnum"; };
     };
 
     services.caddy.virtualHosts."ldap.stu-dev.me".extraConfig = ''
@@ -47,9 +82,13 @@ in
     services.authelia.instances.main = {
       enable = true;
 
-      secrets.storageEncryptionKeyFile = config.age.secrets.authelia-storage-encryption-key.path;
-      secrets.jwtSecretFile = config.age.secrets.authelia-jwt-secret.path;
-      secrets.sessionSecretFile = config.age.secrets.authelia-session-secret.path;
+      secrets = {
+        storageEncryptionKeyFile = config.age.secrets.authelia-storage-encryption-key.path;
+        jwtSecretFile = config.age.secrets.authelia-jwt-secret.path;
+        sessionSecretFile = config.age.secrets.authelia-session-secret.path;
+        oidcHmacSecretFile  = config.age.secrets.authelia-hmac-secret.path;
+        oidcIssuerPrivateKeyFile = config.age.secrets.authelia-private-key.path;
+      };
 
       settings = {
         theme = "auto";
@@ -94,6 +133,20 @@ in
             policy =  "one_factor";
           }];
         };
+
+        identity_providers.oidc.clients = mapAttrsToList (id: cfg: {
+          client_id = id;
+          client_name = cfg.name;
+          client_secret = cfg.secret;
+
+          inherit (cfg) authorization_policy scopes redirect_uris;
+
+          public = false;
+          require_pkce = true;
+          pkce_challenge_method = "S256";
+          userinfo_signed_response_alg = "none";
+          token_endpoint_auth_method = "client_secret_basic";
+        }) cfg.clients;
       };
 
       environmentVariables = {

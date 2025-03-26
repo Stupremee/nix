78712bf4a1f2f6d30d2712d9e81b8def8012f560
Justus K <justus.k@protonmail.com>
2025-03-26 11:28:21 +0100

    hyprlock: initialize proper theme and display

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -6,6 +6,8 @@
 }:
 with lib;
 let
+  inherit (flake.inputs) self;
+
   cfg = config.my.hyprland;
 
   monitorOpts =
@@ -188,6 +190,81 @@ in
 
     programs.hyprlock = {
       enable = true;
+
+      settings = {
+        general = {
+          disable_loading_bar = true;
+          immediate_render = true;
+          hide_cursor = false;
+          no_fade_in = true;
+        };
+
+        background = [
+          {
+            monitor = "";
+            path = self + /assets/wallpaper.png;
+            blur_passes = 3;
+            blur_size = 12;
+            noise = "0.1";
+            contrast = "1.3";
+            brightness = "0.2";
+            vibrancy = "0.5";
+            vibrancy_darkness = "0.3";
+          }
+        ];
+
+        input-field = [
+          {
+            monitor = "eDP-1";
+
+            size = "300, 50";
+            valign = "bottom";
+            position = "0%, 10%";
+
+            outline_thickness = 1;
+
+            font_color = "rgb(b6c4ff)";
+            outer_color = "rgba(180, 180, 180, 0.5)";
+            inner_color = "rgba(200, 200, 200, 0.1)";
+            check_color = "rgba(247, 193, 19, 0.5)";
+            fail_color = "rgba(255, 106, 134, 0.5)";
+
+            fade_on_empty = false;
+            placeholder_text = "Enter Password";
+
+            dots_spacing = 0.2;
+            dots_center = true;
+            dots_fade_time = 100;
+
+            shadow_color = "rgba(0, 0, 0, 0.1)";
+            shadow_size = 7;
+            shadow_passes = 2;
+          }
+        ];
+
+        label = [
+          {
+            monitor = "";
+            text = ''
+              cmd[update:1000] echo "<span font-weight='ultralight' >$(date +'%H %M %S')</span>"
+            '';
+            font_size = 300;
+            font_family = "monospace";
+
+            color = "rgb(b6c4ff)";
+
+            position = "0%, 2%";
+
+            valign = "center";
+            halign = "center";
+
+            shadow_color = "rgba(0, 0, 0, 0.1)";
+            shadow_size = 20;
+            shadow_passes = 2;
+            shadow_boost = 0.3;
+          }
+        ];
+      };
     };
   };
 }
